98e167389432ca8edefaa05d39b062f65f4c7894
alvarito1999 <oj.alv99@gmail.com>
2024-10-28 16:49:55 -0300

    feat(Ejercicio 1): Mejora

--- src/components/Ejercicio1.jsx
+++ src/components/Ejercicio1.jsx
@@ -3,6 +3,25 @@
 
 import { useState } from "react"
 
+const InfoProducto = ({ producto }) => {
+	if (producto) {
+		const { idProducto, nombreProducto, precio } = producto
+		return (
+			<div id="infoProducto">
+				<h3>Informacion del Producto</h3>
+				<p>ID: {idProducto}</p>
+				<p>NOMBRE: {nombreProducto}</p>
+				<p>PRECIO: ${precio}</p>
+			</div>
+		)
+	} else if (producto === null) {
+		return (<p>Aqui se veran los detalles del producto</p>)
+	} else if (producto === undefined) {
+		return (<p>Producto inexistente</p>)
+	}
+
+}
+
 export const Ejercicio1 = ({ productos }) => {
 
 	const encontrarProducto = (idProducto) => {
@@ -12,20 +31,24 @@ export const Ejercicio1 = ({ productos }) => {
 	}
 
 	const [campoBusqueda, setCampoBusqueda] = useState("")// almacena el valor del campo de busqueda
-
+	const [producto, setProducto] = useState(null)
 	const manejarSubmit = (evento, campoBusqueda) => {
 		evento.preventDefault();//previene el submit del formulario
 		evento.target.elements["campoBusqueda"].value = "" //limpia el campo de busqueda
-		console.log(encontrarProducto(campoBusqueda)) //ejecuta la funcion encontrar producto e imprime el resultado en consola
+		setProducto(encontrarProducto(campoBusqueda)) //ejecuta la funcion encontrar producto e imprime el resultado en consola
+
 	}
 
 	return (
 		<section id="Ejercicio-1">
+			<h2>Ejercicio 1</h2>
+			<p>Escribe el ID del producto que deseas encontrar</p>
 			<form
-				onSubmit={(evento) => manejarSubmit(evento, campoBusqueda)}>
+				onSubmit={(evento) => manejarSubmit(evento, campoBusqueda)}
+				autoComplete="off">
 				<input type="text" name="campoBusqueda" id="" placeholder="ingresa el id del pruducto" onChange={(event) => { setCampoBusqueda(event.target.value) }} />
 				<input type="submit" name="submit" id="" value="buscar" />
-				<div></div>
+				<InfoProducto producto={producto} />
 			</form>
 		</section>
 	)
