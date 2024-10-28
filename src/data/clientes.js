f4bf0b1a590e42f868cf9f8c66b23f956b817f3e
alvarito1999 <oj.alv99@gmail.com>
2024-10-28 11:59:17 -0300

    data: clientes

---
+++ src/data/clientes.js
@@ -0,0 +1,5 @@
+export const clientes = JSON.stringify([
+  { idCliente: 1, nombreCliente: "Carlos Ruiz" },
+  { idCliente: 2, nombreCliente: "Lucía Gómez" },
+  { idCliente: 3, nombreCliente: "Martín Díaz" },
+]);
