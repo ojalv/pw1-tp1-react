8eb2a9f60bae3898b941b29552db77667079b277
alvarito1999 <oj.alv99@gmail.com>
2024-10-28 11:58:54 -0300

    data: productos

---
+++ src/data/productos.js
@@ -0,0 +1,7 @@
+export const productos = JSON.stringify([
+  { idProducto: 1, nombreProducto: "Laptop", precio: 1500 },
+  { idProducto: 2, nombreProducto: "Teclado", precio: 50 },
+  { idProducto: 3, nombreProducto: "Mouse", precio: 30 },
+  { idProducto: 4, nombreProducto: "Monitor", precio: 200 },
+  { idProducto: 5, nombreProducto: "Impresora", precio: 120 },
+]);
